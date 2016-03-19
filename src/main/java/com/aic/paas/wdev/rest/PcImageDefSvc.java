81edfaeaebbafb874ff9076ec471b93fc412737d
haoyh1 <haoyh@asiainfo.com>
2016-03-19 10:01:08 +0800

    前场添加PcImageDefSvc

---
+++ src/main/java/com/aic/paas/wdev/rest/PcImageDefSvc.java
@@ -0,0 +1,19 @@
+package com.aic.paas.wdev.rest;
+
+import java.util.List;
+
+import com.aic.paas.wdev.bean.CPcImageDef;
+import com.aic.paas.wdev.bean.PcImageDef;
+
+public interface PcImageDefSvc {
+	
+	/**
+	 * aic.tsd_hyh  2016.03.19
+	 * 根据状态 CPcImageDef 去查 PC_IMAGE_DEF表
+	 * @param CPcBuildTask
+	 * @param orders
+	 * @return
+	 */
+	public List<PcImageDef> selectTaskListByCdt(CPcImageDef cid,String orders);
+
+}
