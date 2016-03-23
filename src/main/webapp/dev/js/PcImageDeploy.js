1224d436dc3c879b89b20ad3e8396263ef6e5a1a
renfeng <renfeng@renfeng-PC>
2016-03-23 11:07:10 +0800

    ok

--- src/main/webapp/dev/js/PcImageDeploy.js
+++ src/main/webapp/dev/js/PcImageDeploy.js
@@ -117,10 +117,7 @@ function toTreeData(dcs, rcs) {
 			}else{
 				pnmap["env"+envType][rcs[i].parentCode] = CU.clone(dcmap[rcs[i].parentCode]);
 			}
-<<<<<<< HEAD
-=======
-			
->>>>>>> refs/heads/PAAS-FE-DEV-FEATURE-V1.0.0
+
 		}
 		
 		rcs[i].id = rcs[i].code;
